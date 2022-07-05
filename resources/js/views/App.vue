5115ca157de6abc09d5db2e4c08090ffeee95c27
GRND0 <zefiro.turchetta@gmail.com>
2022-07-05 17:18:16 +0200

    scaffold file js

---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>
+            Front Office
+        </h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+   name: 'App' 
+}
+
+</script>
+
+<style>
+
+
+</style>
